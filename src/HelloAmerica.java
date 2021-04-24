28047a07a71d12d14dc1137f36e67bfb3b998104
Orkhan <jkrogers222@gmail.com>
2021-04-24 17:08:02 -0400

    second commit

---
+++ src/HelloAmerica.java
@@ -0,0 +1,9 @@
+public class HelloAmerica {
+    public static void main(String[] args) {
+
+        System.out.println("Hello America");
+        System.out.println("Freedom and wellness");
+
+
+    }
+}
